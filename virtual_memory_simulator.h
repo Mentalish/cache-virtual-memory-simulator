75e705c95e0f51d80977bdd05a6e4197c9c3dd5b
Erik <44383316+Mentalish@users.noreply.github.com>
2026-04-08 23:54:33 -0500

    simulation function prototypes

---
+++ virtual_memory_simulator.h
@@ -0,0 +1,17 @@
+#ifndef VIRTUAL_MEMORY_SIMULATION_H
+#define VIRTUAL_MEMORY_SIMULATION_H
+#include "memory_calculations.h"
+#include "page_table.h"
+
+typedef struct {
+	int totalPages;
+	int pagesAvaibletoUser;
+	int pageHits;
+	int pagesFree;
+	int pageFaults;
+} MemorySimulationResults;
+
+int runVirtualMemorySimulation(Process **processes,
+										 MemoryCalculationResults *pgTableParameters);
+
+#endif
